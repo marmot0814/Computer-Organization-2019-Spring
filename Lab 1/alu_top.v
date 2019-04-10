f8e3ebbe105a2e046e5be390f1cc5710b62969cf
marmot0814 <s1357andy871207@gmail.com>
2019-04-10 10:29:27 +0800

    Initial Commit

---
+++ Lab 1/alu_top.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ps
+
+module alu_top(
+               src1,       //1 bit source 1 (input)
+               src2,       //1 bit source 2 (input)
+               less,       //1 bit less     (input)
+               A_invert,   //1 bit A_invert (input)
+               B_invert,   //1 bit B_invert (input)
+               cin,        //1 bit carry in (input)
+               operation,  //operation      (input)
+               result,     //1 bit result   (output)
+               cout,       //1 bit carry out(output)
+               );
+
+input         src1;
+input         src2;
+input         less;
+input         A_invert;
+input         B_invert;
+input         cin;
+input [2-1:0] operation;
+
+output        result;
+output        cout;
+
+reg           result;
+
+always@( * )
+begin
+
+end
+
+endmodule
