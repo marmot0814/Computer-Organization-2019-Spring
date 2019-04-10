31bc5c7c7cfc9e132566140ed1963e18cdde6607
marmot0814 <s1357andy871207@gmail.com>
2019-04-10 12:13:57 +0800

    replace with full adder

--- Lab 1/alu_top.v
+++ Lab 1/alu_top.v
@@ -30,36 +30,34 @@ output          cout;
 output          set;
 
 reg             result;
-reg             set;
-reg             cout;
-reg	            xor_value;
-reg	            a_tmp;
-reg	            b_tmp;
+reg             xor_value;
+reg             a_tmp;
+reg             b_tmp;
 
 wire            comp_result;
+wire            set;
+wire            cout;
 
 generate
     compare cmp(less, equal, comp, comp_result);
+    full_adder fa(a_tmp, b_tmp, cin, set, cout);
 endgenerate
 
 always@( * ) begin
-	case (A_invert)
-		1'b0:	a_tmp =  src1;
-		1'b1:	a_tmp = ~src1;
-	endcase
-	case (B_invert)
-		1'b0:	b_tmp =  src2;
-		1'b1:	b_tmp = ~src2;
-	endcase
-	xor_value = (a_tmp ^ b_tmp);
-    set = xor_value ^ cin;
-	cout   = (a_tmp & b_tmp) | (xor_value & cin);
-	case (operation)
-		2'b00:  result = a_tmp & b_tmp;
-		2'b01:	result = a_tmp | b_tmp;
-		2'b10:  result = set;
-		2'b11:	result = comp_result;
-	endcase
+    case (A_invert)
+        1'b0:   a_tmp =  src1;
+        1'b1:   a_tmp = ~src1;
+    endcase
+    case (B_invert)
+        1'b0:   b_tmp =  src2;
+        1'b1:   b_tmp = ~src2;
+    endcase
+    case (operation)
+        2'b00:  result = a_tmp & b_tmp;
+        2'b01:  result = a_tmp | b_tmp;
+        2'b10:  result = set;
+        2'b11:  result = comp_result;
+    endcase
 end
 
 endmodule
