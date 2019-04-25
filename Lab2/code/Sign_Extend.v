51046e9cf5eb11f0f8be6acad9e2ccb35c791df9
marmot0814 <s1357andy871207@gmail.com>
2019-04-26 01:12:15 +0800

    Sign_Extend done

--- Lab2/code/Sign_Extend.v
+++ Lab2/code/Sign_Extend.v
@@ -12,16 +12,20 @@
 module Sign_Extend(
     data_i,
     data_o
-    );
+);
                
 //I/O ports
-input   [16-1:0] data_i;
-output  [32-1:0] data_o;
+input   [16-1:0]    data_i;
+output  [32-1:0]    data_o;
 
 //Internal Signals
-reg     [32-1:0] data_o;
+reg     [32-1:0]    data_o;
 
 //Sign extended
+always @(*) begin
+    data_o[16-1:0]  = data_i;
+    data_o[32-1:16] = {16{data_i[15]}};
+end
           
 endmodule      
      
