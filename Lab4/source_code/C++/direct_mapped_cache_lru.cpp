fd86b621972c745770c415f87f2177df0eb9a556
marmot0814 <s1357andy871207@gmail.com>
2019-05-31 11:42:49 +0800

    lru done

---
+++ Lab4/source_code/C++/direct_mapped_cache_lru.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <iomanip>
+#include <cmath>
+
+#define FILENAME "../../test/LU.txt"
+
+using namespace std;
+
+struct cache_content {
+    int stamp;
+    unsigned int tag;
+};
+
+const int K = 1024;
+const int block_size = 64;
+const int INF = 0x3f3f3f3f;
+
+double simulate(int cache_size, int way) {
+    int offset_bit = __lg(block_size);
+    int line = cache_size >> offset_bit;
+    int index_bit  = __lg(line) - __lg(way);
+    
+    cache_content *cache = new cache_content[line];
+
+    for (int i = 0 ; i < line ; i++)
+        cache[i].stamp = -1;
+
+    FILE *fp = fopen(FILENAME, "r");
+
+    unsigned int total = 0, miss = 0;
+    int x; while (~fscanf(fp, "%x", &x)) {
+        total++;
+        x >>= offset_bit;
+        unsigned int index = x & ((1 << index_bit) - 1);
+        unsigned int tag   = x >> index_bit;
+
+        bool hit = false;
+        for (int i = 0 ; i < way ; i++) {
+            if (~cache[index + i].stamp && cache[index + i].tag == tag)
+                hit = true;
+        }
+        if (hit) continue;
+        miss++;
+        int target_i = -1, minV = INF;
+        for (int i = 0 ; i < way ; i++)
+            if (cache[index + i].stamp < minV)
+                minV = cache[index + i].stamp, target_i = i;
+        cache[index + target_i].stamp = total;
+        cache[index + target_i].tag = tag;
+    }
+    fclose(fp);
+    return 100.0 * miss / total;
+}
+
+int main() {
+    cout << "      ";
+    for (int way = 1 ; way <= 8 ; way <<= 1)
+        cout << setw(4) << way << "-way";
+    cout << "\n---------------------------------------\n";
+    for (int cache_size = 1 ; cache_size <= 32 ; cache_size <<= 1) {
+        cout << setw(3) << cache_size << "K: ";
+        for (int way = 1 ; way <= 8 ; way <<= 1)
+            cout << setw(8) << fixed << setprecision(3) << simulate(cache_size * K, way);
+        cout << '\n';
+    }
+}
